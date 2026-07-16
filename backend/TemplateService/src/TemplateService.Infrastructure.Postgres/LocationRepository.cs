65d8bca4768544ba3ea1e378f32ed61e47593f44
vit-vokhminov <vit-vokhminov@yandex.ru>
2026-07-16 09:07:32 +0700

    fix

--- backend/TemplateService/src/TemplateService.Infrastructure.Postgres/LocationRepository.cs
+++ backend/TemplateService/src/TemplateService.Infrastructure.Postgres/LocationRepository.cs
@@ -41,18 +41,6 @@ public sealed partial class LocationRepository : ILocationRepository
     {
         return await _dbContext.Locations
         .AnyAsync(l => l.Name.Value == name, cancellationToken);
-        // post/api/Locations
-// {
-//   "name": "test",
-//   "address": {
-//     "street": "test",
-//     "city": "test",
-//     "zipCode": null,
-//     "country": "test"
-//   }
-// }
-        // System.InvalidOperationException: "The LINQ expression 'DbSet<Location>()
-    //.Any(l => l.Name.Value == @name)' could not be translated. Either rewrite the query in a form that can be translated, or switch to client evaluation explicitly by //inserting a call to 'AsEnumerable', 'AsAsyncEnumerable', 'ToList', or 'ToListAsync'. See https://go.microsoft.com/fwlink/?linkid=2101038 for more information."
     }
 
     [LoggerMessage(LogLevel.Error, "Ошибка сохранения локации с именем {Name}")]
